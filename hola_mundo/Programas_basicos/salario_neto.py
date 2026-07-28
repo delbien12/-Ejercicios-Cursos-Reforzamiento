5c8bddb949a8e6f5af948b61262ea5d9b5b9a969
delbien12 <antoniotom132@gmail.com>
2026-07-27 19:56:36 -0600

    mi salario

---
+++ hola_mundo/Programas_basicos/salario_neto.py
@@ -0,0 +1,5 @@
+# Programa para calcular el salario neto
+salario_bruto = float(input("Ingrese el salario bruto: "))
+descuentos = float(input("Ingrese los descuentos: "))
+salario_neto = salario_bruto - descuentos
+print("El salario neto es:", salario_neto)
