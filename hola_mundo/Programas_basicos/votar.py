0a4ec9e0360600a6b1581ae6dea6db05d41a5d53
delbien12 <antoniotom132@gmail.com>
2026-07-29 17:53:29 -0600

    votar

---
+++ hola_mundo/Programas_basicos/votar.py
@@ -0,0 +1,7 @@
+# Programa que determina si una persona puede votar según su edad.
+edad = int(input("¿Cuál es tu edad? "))
+
+if edad >= 18:
+    print("Sí puede votar")
+else:
+    print("No puede votar")
